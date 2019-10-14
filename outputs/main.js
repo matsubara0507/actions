248f6a441172ba8d66b9aa0067469d7a97e91273
MATSUBARA Nobutada <t12307043@gunma-u.ac.jp>
2019-10-14 17:15:48 +0900

    Fix: await

--- outputs/main.js
+++ outputs/main.js
@@ -3,10 +3,14 @@ const exec = require('@actions/exec');
 
 try {
   const script = core.getInput('script');
-  await exec.exec(script, stdout: (data: Buffer) => {
-    const result = data.toString();
-    core.setOutput("result", result);
-  });
+  const options = {};
+  options.listeners = {
+    stdout: (data: Buffer) => {
+      const result = data.toString();
+      core.setOutput("result", result);
+    }
+  };
+  await exec.exec(script, options);
 } catch (error) {
   core.setFailed(error.message);
 }
